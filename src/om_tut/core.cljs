6342d098c897e2223288adaefacf9f8dc972b301
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-05 00:53:25 -0700

    Contact list!

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -7,35 +7,47 @@
 ;; define your app data so that it doesn't get over-written on reload
 ;; It must consist of associative data structures -- map or indexed sequential
 ;; data like a vector, but NOT a set, list, or lazy sequence.
-(defonce app-state (atom {:list ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+(defonce app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]})) 
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+               (dom/h2 nil "Contact List")
+               (apply dom/ul nil
+                      (om/build-all contact-view (:contacts data)))))))
+
 
 (defn reload-page 
   "Refreshes the page"
   []
   (.reload (.-location js/window)))
 
-(defn stripe [el text bgc]
-  (let [st #js {:backgroundColor bgc}]
-    (el #js {:style st} text)))
-
-;; Nice because it has the style info in the middle, more closely mimicking actual elements.
-(defn stripe2 [el bgc text]
-  (let [st #js {:backgroundColor bgc}]
-    (el #js {:style st} text)))
-
-(om/root
-  (fn [data owner]
-    (om/component
-      (apply dom/ul #js {:className "animals"} ; #js is a reader macro for making js objects or arrays -- it is shallow.
-             (map (fn [text] (dom/li nil text)) (:list data))
-             (map (partial dom/li nil) (:list data))
-             (map #(dom/li nil %) (:list data))   
-             (map (fn [text color] (stripe2 dom/li color text)) (:list data) (cycle ["#abc" "#cba"]))
-             (map (partial stripe dom/li) (:list data) (cycle ["#abc" "#cba"]))
-             (map #(stripe2 dom/li %2 %1) (:list data) (cycle ["#abc" "#cba"]))
-             )))
-  app-state
-  {:target (. js/document (getElementById "app-0"))})
+(om/root contacts-view app-state
+         {:target (. js/document (getElementById "app-0"))})
 
 ; (om/root
 ;   (fn [data owner] ; takes app state data, and the backing react component.
