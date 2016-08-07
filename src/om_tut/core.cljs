02b0846a7dd52704460ed19e3bd92a1b3b86be8d
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-07 14:56:14 -0700

    Edit classes everywhere!

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -86,7 +86,7 @@
               (dom/div nil (display-name professor))
               (dom/label nil "Classes")
               (apply dom/ul nil
-                     (map #(dom/li nil (om/value %)) (:classes professor)))))))
+                     (map #(dom/li nil (om/build editable %)) (:classes professor)))))))
 
 (defmulti entry-view (fn [person _] (:type person)))
 
