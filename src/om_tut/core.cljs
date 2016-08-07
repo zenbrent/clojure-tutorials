3c9215c8d0562cbb67ba586524e9c69e3301f71e
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-07 14:08:25 -0700

    Higher order components

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,29 +1,26 @@
 (ns ^:figwheel-always om-tut.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]
             [clojure.string :as string]))
-
-;; https://github.com/omcljs/om/wiki/Basic-Tutorial#enhancing-your-first-om-component
-
-;; lighttable notes!
-;; <C-space> pulls up the command pane. sups useful.
 
 (enable-console-print!)
 
-;; define your app data so that it doesn't get over-written on reload
-;; It must consist of associative data structures -- map or indexed sequential
-;; data like a vector, but NOT a set, list, or lazy sequence.
-(defonce app-state
+(def app-state
   (atom
-    {:contacts
-     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]})) 
+    {:people
+     [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker"
+       :email "aphacker@mit.edu"}
+      {:type :professor :first "Gerald" :middle "Jay" :last "Sussman"
+       :email "metacirc@mit.edu" :classes [:6001 :6946]}
+      {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu"
+       :classes [:6001]}]
+     :classes
+     {:6001 "The Structure and Interpretation of Computer Programs"
+      :6946 "The Structure and Interpretation of Classical Mechanics"
+      :1806 "Linear Algebra"}}))
 
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
@@ -33,87 +30,50 @@
 (defn display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
 
-(defn parse-contact [contact-str]
-  (let [[first middle last :as parts] (string/split contact-str #"\s+")
-        [first last middle] (if (nil? last) [first middle] [first last middle])
-        middle (when middle (string/replace middle "." "")) ;; e.g. Brent M. Brimhall
-        c (if middle (count middle) 0)]
-    (when (>= (count parts) 2)
-      (cond-> {:first first :last last}
-        (== c 1) (assoc :middle-initial middle)
-        (>= c 2) (assoc :middle middle))))) 
-
-(defn add-contact [data owner]
-  (let [new-contact (-> (om/get-node owner "new-contact")
-                        .-value
-                        parse-contact)]
-    (when new-contact
-      (om/transact! data :contacts #(conj % new-contact))
-      (om/set-state! owner :text ""))))
-
-(defn contact-view [contact owner]
+(defn student-view [student owner]
   (reify
-    om/IRenderState
-    (render-state [this {:keys [delete]}]
+    om/IRender
+    (render [_]
+      (dom/li nil (display-name student)))))
+
+(defn professor-view [professor owner]
+  (reify
+    om/IRender
+    (render [_]
       (dom/li nil
-              (dom/span nil (display-name contact))
-              (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
+              (dom/div nil (display-name professor))
+              (dom/label nil "Classes")
+              (apply dom/ul nil
+                     (map #(dom/li nil %) (:classes professor)))))))
 
+(defmulti entry-view (fn [person _] (:type person)))
 
-;; If you are familiar with React you'll notice that this is a little bit clunkier than
-;; in React, here we have to make sure to set the state of the text field even if we don't
-;; want it to change. This is a side effect of React's internals clashing a bit with Om's
-;; optimization of always rendering on requestAnimationFrame.
-(defn handle-change [e owner {:keys [text]}]
-  (let [value (.. e -target -value)]
-    (if-not (re-find #"[0-9]" value)
-      (om/set-state! owner :text value)
-      (om/set-state! owner :text text))))
+(defmethod entry-view :student
+  [person owner] (student-view person owner))
 
-(defn contacts-view [data owner]
+(defmethod entry-view :professor
+  [person owner] (professor-view person owner))
+
+(defn people [data]
+  (->> data
+       :people
+       (mapv (fn [x]
+               (if (:classes x)
+                 (update-in x [:classes] ;; nested update
+                            (fn [cs] (mapv (:classes data) cs)))
+                 x)))))
+
+(defn registry-view [data owner]
   (reify
-    om/IInitState
-    (init-state [_]
-      {:delete (chan)
-       :text ""})
-    om/IWillMount
-    (will-mount [_]
-      (let [delete (om/get-state owner :delete)]
-        (go (loop []
-              (let [contact (<! delete)]
-                (om/transact! data :contacts
-                              ;; using vec to transform the result of a lazy sequence to a vector
-                              ;; because state can only consist of associative data like maps & vectors
-                              (fn [xs] (vec (remove #(= contact %) xs))))
-                (recur))))))
     om/IRenderState
-    (render-state [this state]
-      (dom/div nil
-               (dom/h2 nil "Contact List")
+    (render-state [_ state]
+      (dom/div #js {:id "registry"}
+               (dom/h2 nil "Registry")
                (apply dom/ul nil
-                      (om/build-all contact-view (:contacts data)
-                                    {:init-state state}))
-               (dom/div nil
-                        (dom/input #js {:type "text" :ref "new-contact" :value (:text state)
-                                        :onChange #(handle-change % owner state)})
-                        (dom/button #js {:onClick #(add-contact data owner)} "Add contact"))))))
+                      (om/build-all entry-view (people data)))))))
 
-
-(defn reload-page 
-  "Refreshes the page"
-  []
-  (.reload (.-location js/window)))
-
-(om/root contacts-view app-state
-         {:target (. js/document (getElementById "app-0"))})
-
-; (om/root
-;   (fn [data owner] ; takes app state data, and the backing react component.
-;     (reify om/IRender ; must return an Om component. om.core/component macro generates om/IRenders.
-;       (render [_]
-;         (dom/div nil (:text data)))))
-;   app-state
-;   {:target (. js/document (getElementById "app-1"))})
+(om/root registry-view app-state
+  {:target (. js/document (getElementById "registry"))})
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
